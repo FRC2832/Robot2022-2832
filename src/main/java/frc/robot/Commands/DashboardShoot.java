67f6146e1f493bf79ed228fc1cdc5cb37b449550
scarmain <scarmain@gmail.com>
2022-02-02 21:15:26 -0500

    Shooter setup to shoot on robot

--- src/main/java/frc/robot/Commands/DashboardShoot.java
+++ src/main/java/frc/robot/Commands/DashboardShoot.java
@@ -2,10 +2,20 @@ package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Shooter;
 
-public class DashboardShoot extends CommandBase{
+public class DashboardShoot extends CommandBase {
+    private Shooter shooter;
+
+    public DashboardShoot(Shooter shooter) {
+        this.shooter = shooter;
+        addRequirements(shooter);
+        SmartDashboard.putNumber("Target RPM", 2000);
+    }
+    
     @Override
     public void execute() {
-        double rpm = SmartDashboard.getNumber("Target RPM", 1000);
+        double rpm = SmartDashboard.getNumber("Target RPM", 2000);
+        shooter.setShooterRpm(rpm);
     }
 }
---
+++ src/main/java/frc/robot/Commands/NoShoot.java
@@ -0,0 +1,18 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Shooter;
+
+public class NoShoot extends CommandBase{
+    private Shooter shooter;
+
+    public NoShoot(Shooter shooter) {
+        this.shooter = shooter;
+        addRequirements(shooter);
+    }
+    
+    @Override
+    public void execute() {
+        shooter.setShooterRpm(0);
+    }
+}
