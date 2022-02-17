127f1d0cbe83d18af01d2698f11f6f58e7a7a8ea
Anna Novak <77454079+anovak4@users.noreply.github.com>
2022-02-16 19:07:28 -0500

    update target picker equation

--- src/main/java/frc/robot/Pi.java
+++ src/main/java/frc/robot/Pi.java
@@ -95,10 +95,10 @@ public class Pi {
         sortTargets();
         // pick a target just right of center so the cargo hopefully doesn't bounce out
         int index = 0;
-        if(targetCenterXArray.length <= 3) {
-            index = targetCenterXArray.length - 1;
+        if (targetCenterXArray.length <= 1) {
+            index = 0; 
         } else {
-            index = (int) ((targetCenterXArray.length / 2) + 1);
+            index = (int) Math.ceil((double) targetCenterXArray.length / 2);
         }
         double targetX = (double) targetCenterXArray[index];
         if (targetX < (CAM_X_RES / 2) - (CAM_X_RES * 0.05)) {
