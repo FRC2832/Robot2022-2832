d662c161a1a6a747795eda5e864b0fa9f840b3a0
Anna Novak <77454079+anovak4@users.noreply.github.com>
2022-02-23 19:18:28 -0500

    added shooter constants

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.*;
+import frc.robot.ShooterConstants;
 
 public class Robot extends TimedRobot {
     private final XboxController controller = new XboxController(0);
