795baa7f2ed3cdc0911a7b46ce54d68f65232e75
sethp18 <spianga@livoniawarriors.org>
2022-02-26 15:53:46 -0500

    ODOMETRY
    set up example autonomous (Auton 0) implementing odometry.

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -49,9 +49,9 @@ public class Drivetrain extends SubsystemBase {
     private ADXRS450_GyroSim gyroSim;
     private ADXRS450_Gyro gyroBase;
 
-    private final SwerveDriveKinematics kinematics;
+    private SwerveDriveKinematics kinematics;
 
-    private final SwerveDriveOdometry odometry;
+    public SwerveDriveOdometry odometry;
 
     private Field2d field = new Field2d();
 
@@ -364,4 +364,29 @@ public class Drivetrain extends SubsystemBase {
         }
         return new Transform2d(trans, (new Rotation2d(angle)).minus(robot.getRotation()));
     }
+    public int currentStep = 0;
+    public void setPosition(double xDesPosition, double yDesPosition, double desRotation, double time, int step){
+        Pose2d pos = odometry.getPoseMeters();
+        double xCurrentPos = pos.getX();
+        double yCurrentPos = pos.getY();
+        double rotCurrentPos = pos.getRotation().getRadians();
+        double xMove = xDesPosition-xCurrentPos;
+        double yMove = yDesPosition-yCurrentPos;
+        double rotMag = rotCurrentPos % 2*Math.PI;
+        rotMag = desRotation-rotCurrentPos;
+
+        if(step == currentStep){
+            if(Math.abs(xCurrentPos-xDesPosition) > 0.1 && Math.abs(yCurrentPos-yDesPosition) > 0.1){
+                drive(xMove/time, yMove/time, rotMag/time, true);
+                System.out.println("Its working");
+            }
+            else{
+                drive(0, 0, 0, false);
+                System.out.println("Arrived");
+                currentStep++;
+            }
+        }
+
+    } 
+
 }
---
+++ src/main/java/frc/robot/Odometry.java
@@ -0,0 +1,65 @@
+package frc.robot;
+
+import frc.robot.Drivetrain;
+import frc.robot.SwerveModule;
+import frc.robot.SwerveConstants;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Pose2d;
+
+public class Odometry{
+
+    SwerveConstants constants = new SwerveConstants();
+    SwerveModule swerveModule = new SwerveModule(constants);
+    Drivetrain drivetrain = new Drivetrain();
+
+    Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
+    Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
+    Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
+    Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
+
+
+    SwerveDriveKinematics kinematics = new SwerveDriveKinematics(m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+
+    
+
+    SwerveDriveOdometry swerveOdometry = new SwerveDriveOdometry(kinematics, drivetrain.getHeading());
+    
+    public double getXPosition(){
+        double xPos = swerveOdometry.getPoseMeters().getX();
+        return xPos;
+    }
+    
+    public double getYPosition(){
+        double yPos = swerveOdometry.getPoseMeters().getY();
+        return yPos;
+    }
+    
+    public Rotation2d getRot(){
+        Rotation2d rotation2d = drivetrain.getHeading();
+        return rotation2d;
+    }
+    
+    public void setPosition(double xPosition, double yPosition, double rotation, double time){
+        double xPos = swerveOdometry.getPoseMeters().getX();
+        double yPos = swerveOdometry.getPoseMeters().getY();
+        double rot = swerveOdometry.getPoseMeters().getRotation().getRadians();
+        xPos = xPosition-xPos;
+        yPos = yPosition-yPos;
+        rot = rot % 2*Math.PI;
+        rot = rotation-rot;
+        //while(swerveOdometry.getPoseMeters().getX()!=xPosition || swerveOdometry.getPoseMeters().getY()!=yPosition){
+            drivetrain.drive(xPos/time, yPos/time, rot/time, false);
+         //   }
+       // drivetrain.drive(0, 0, 0, false);
+
+    } 
+
+    public void setPositionv2(SwerveModuleState setState){
+        SwerveModuleState swerveState = setState;
+        
+    }
+}
