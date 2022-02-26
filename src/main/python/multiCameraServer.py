ef2b976601ff305cfe2b4881c2bc8a8615cab631
Anna Novak <77454079+anovak4@users.noreply.github.com>
2022-02-25 19:45:04 -0500

    fix array passing problem
    small fix for the problem with array lengths changing while publishing to network tables

--- src/main/python/multiCameraServer.py
+++ src/main/python/multiCameraServer.py
@@ -434,13 +434,15 @@ def extra_target_processing(pipeline):
     print('target width', target_widths)
     print('target height', target_heights)
     print('target area', target_areas)
-    # Publish to the '/vision/red_areas' network table
-    table = NetworkTables.getTable('vision')
-    table.putNumberArray('targetX', target_x_positions)
-    table.putNumberArray('targetY', target_y_positions)
-    table.putNumberArray('targetWidth', target_widths)
-    table.putNumberArray('targetHeight', target_heights)
-    table.putNumberArray('targetArea', target_areas)
+    # i know this looks disgusting but it works. A for loop would be neater but would it affect the 20ms loop?
+    if len(target_x_positions) == len(target_y_positions) and len(target_x_positions) == len(target_widths) and len(target_x_positions) == len(target_heights) and len(target_x_positions) == len(target_areas): 
+        # Publish to the '/vision/red_areas' network table
+        table = NetworkTables.getTable('vision')
+        table.putNumberArray('targetX', target_x_positions)
+        table.putNumberArray('targetY', target_y_positions)
+        table.putNumberArray('targetWidth', target_widths)
+        table.putNumberArray('targetHeight', target_heights)
+        table.putNumberArray('targetArea', target_areas)
 
 
 
