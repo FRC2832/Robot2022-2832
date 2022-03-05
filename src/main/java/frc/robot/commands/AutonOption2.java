8fde49c8dc677bbd411eccdaeeadcee757558e55
Anna Novak <77454079+anovak4@users.noreply.github.com>
2022-03-05 15:54:14 -0500

    small update

--- src/main/java/frc/robot/commands/AutonOption2.java
+++ src/main/java/frc/robot/commands/AutonOption2.java
@@ -3,45 +3,31 @@ package frc.robot.commands;
 import frc.robot.Drivetrain;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class AutonOption2 extends CommandBase {
-    // private Timer timer;
     private Drivetrain drive;
-    // private double delay = 4.0;
 
     public AutonOption2(Drivetrain drive) {
-        // timer = new Timer();
         this.drive = drive;
         addRequirements(drive);
-        // delay = SmartDashboard.getNumber("Shooting delay", 0.0);
-        // timer.start();
         drive.currentStep = 0;
     }
 
-    public void execute() {
-
-        if(drive.currentStep == 0) {
-            drive.odometry.resetPosition(new Pose2d(6.5, 5.0, new Rotation2d()), new Rotation2d());
-            drive.currentStep++;
-        }
-
+    public void initialize() {
         // TODO: what happens if we start on the equivalent red side of the field?
-        drive.setPosition(5.06, 5.96, Math.toRadians(135), 0.5, 1); // to blueBalls[0]
+        drive.odometry.resetPosition(new Pose2d(6.5, 5.0, new Rotation2d()), new Rotation2d());
+        drive.currentStep++;
+    }
 
+    public void execute() {
+        drive.setPosition(5.06, 5.96, Math.toRadians(135), 0.5, 1); // to blueBalls[0]
         // ingest
         // shoot - CommandScheduler.getInstance().schedule(new AutoShoot());?
-
         drive.setPosition(5.99, 6.95, Math.toRadians(50), .5, 2); // to redBalls[7]
-
         // ingest
-
         drive.setPosition(4.65, 6.84, Math.toRadians(190), .5, 3); // to hangar
-
         // shoot lightly into hangar - DashboardShoot but with a value of 1000 rpm and 39 degrees hood angle
-
         drive.setPosition(6, 7, 0, .5, 4); //towards center line
     }
 
